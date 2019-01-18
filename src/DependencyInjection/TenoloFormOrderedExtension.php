7bdd481a7bfc80ad73519f4aff281e0af9bbac9e
Nikita Loges <git@loges.one>
2019-01-18 12:16:54 +0100

    stuff

--- src/DependencyInjection/TenoloFormOrderedExtension.php
+++ src/DependencyInjection/TenoloFormOrderedExtension.php
@@ -1,24 +1,19 @@
 <?php
-/*
- * This file is part of the Ivory Ordered Form bundle package.
- *
- * (c) Eric GELOEN <geloen.eric@gmail.com>
- *
- * For the full copyright and license information, please read the LICENSE
- * file that was distributed with this source code.
- */
 
 namespace Tenolo\Bundle\FormOrderedBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\Form\AbstractType;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Tenolo\FormOrdered\OrderedResolvedFormTypeFactory;
 
 /**
- * @author GeLo <geloen.eric@gmail.com>
+ * Class TenoloFormOrderedExtension
+ *
+ * @package Tenolo\Bundle\FormOrderedBundle\DependencyInjection
+ * @author  GeLo <geloen.eric@gmail.com>
+ * @author  Nikita Loges
+ * @company tenolo GbR
  */
 class TenoloFormOrderedExtension extends Extension
 {
@@ -27,18 +22,7 @@ class TenoloFormOrderedExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
-
-        if (!method_exists(AbstractType::class, 'getBlockPrefix')) {
-
-            $container->getDefinition('tenolo_form_ordered.form_extension')
-                ->clearTag($tag = 'form.type_extension')
-                ->addTag($tag, ['alias' => 'form']);
-
-            $container->getDefinition('tenolo_form_ordered.button_extension')
-                ->clearTag($tag)
-                ->addTag($tag, ['alias' => 'button']);
-        }
     }
 }
--- src/TenoloFormOrderedBundle.php
+++ src/TenoloFormOrderedBundle.php
@@ -1,20 +1,16 @@
 <?php
 
-/*
- * This file is part of the Ivory Ordered Form bundle package.
- *
- * (c) Eric GELOEN <geloen.eric@gmail.com>
- *
- * For the full copyright and license information, please read the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Tenolo\Bundle\FormOrderedBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
- * @author GeLo <geloen.eric@gmail.com>
+ * Class TenoloFormOrderedBundle
+ *
+ * @package Tenolo\Bundle\FormOrderedBundle
+ * @author  GeLo <geloen.eric@gmail.com>
+ * @author  Nikita Loges
+ * @company tenolo GbR
  */
 class TenoloFormOrderedBundle extends Bundle
 {
